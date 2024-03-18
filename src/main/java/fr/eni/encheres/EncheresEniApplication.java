e069422941ebc481ac396d2a2153e1e5457c6ede
Melaine <melaine.favennec2023@campus-eni.fr>
2024-03-18 14:27:32 +0100

    ajout des bo du projet

--- src/main/java/fr/eni/encheres/EncheresEniApplication.java
+++ src/main/java/fr/eni/encheres/EncheresEniApplication.java
@@ -1,18 +1,12 @@
-package com.example.demo;
-
-import java.io.Console;
-
+package fr.eni.encheres;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import com.mysql.cj.log.Log;
 
 @SpringBootApplication
 public class EncheresEniApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EncheresEniApplication.class, args);
-		System.out.println("test");
 	}
 
 }
