1877fa32980b7bd7ea7871e37a864aa698da86f8
Melaine <melaine.favennec2023@campus-eni.fr>
2024-03-21 16:09:10 +0100

    ajout detail article dans config plus modif visuel page login

--- src/main/java/fr/eni/encheres/configuration/WebConfiguration.java
+++ src/main/java/fr/eni/encheres/configuration/WebConfiguration.java
@@ -61,7 +61,7 @@ public class WebConfiguration implements WebMvcConfigurer {
 		http
 		.csrf(csrf ->csrf.ignoringRequestMatchers("**"))
 			.authorizeHttpRequests((requests) -> requests
-				.requestMatchers("/css/**","/images/**","/", "/encheres","/logout","register" ,"/","/resetPasswordValid","/resetPassword","ajout-vente").permitAll()
+				.requestMatchers("/css/**","/images/**","/", "/encheres","/logout","register" ,"/","/resetPasswordValid","/resetPassword","/encheres/detail").permitAll()
 				.requestMatchers("/profil","/modifierProfil","ajout-vente","ajout").hasAnyRole("MEMBRE", "ADMINISTRATEUR")
 				.anyRequest().authenticated()
 			)
