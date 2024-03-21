01e5f35fc013e42a75f718160c87ce2c2b866272
izang2023 <izang2023@ad.campus-eni.fr>
2024-03-21 14:53:36 +0100

    [IZA] Controller & service vente

--- src/main/java/fr/eni/encheres/bll/ArticlesServiceImpl.java
+++ src/main/java/fr/eni/encheres/bll/ArticlesServiceImpl.java
@@ -1,6 +1,8 @@
 package fr.eni.encheres.bll;
 
 import fr.eni.encheres.bo.Article;
+import fr.eni.encheres.bo.Categorie;
+import fr.eni.encheres.bo.Utilisateur;
 import fr.eni.encheres.dal.ArticleDAO;
 import fr.eni.encheres.exceptions.BusinessException;
 
@@ -8,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
+import java.sql.Date;
 import java.util.List;
 
 @Service
@@ -31,6 +34,19 @@ public class ArticlesServiceImpl implements ArticlesService{
 		//Vérification des informations saisies
 		BusinessException be = new BusinessException();
 		boolean isValid = true;
+		int id_article = article.getIdArticle();
+		String nom_article = article.getNom();
+		String description = article.getDescription();
+		Date debut = article.getDebut();
+		Date fin = article.getFin();
+		int prix_intial = article.getMiseAprix();
+		int prix_vente = article.getPrixVente();
+		Utilisateur vendeur = article.getVendeur();
+	    Categorie categorie = article.getCategorie();
+		
+	/*	if(isValid) {
+			articleDAO.ajoutArticle(null, null, null, null, 0, 0, null, null)
+		}*/
 		
 		
 	}
