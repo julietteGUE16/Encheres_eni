726ee74716ace29db79fc39e312a33a3c524586f
Melaine <melaine.favennec2023@campus-eni.fr>
2024-03-28 10:41:19 +0100

    Modification webService

--- src/main/java/fr/eni/encheres/configuration/WebConfiguration.java
+++ src/main/java/fr/eni/encheres/configuration/WebConfiguration.java
@@ -64,11 +64,11 @@ public class WebConfiguration implements WebMvcConfigurer {
 			.authorizeHttpRequests((requests) -> requests
 
 
-				.requestMatchers("/css/**","/js/**","/images/**","/", "/encheres","/logout","/register","register","/registerValid" ,"/**","/resetPasswordValid","/resetPassword","ajout-vente","/ajout-vente-valider", "/encheres/*", "/encheresParCategorieEtNom").permitAll()
+				.requestMatchers("/css/**","/js/**","/images/**","/", "/encheres","/register","register","/registerValid","/resetPasswordValid","/resetPassword","/newPasswordValid","/newPassword", "/encheres/*", "/encheresParCategorieEtNom").permitAll()
 
 
 				
-				.requestMatchers("/profil","/modifierProfil","ajout-vente","ajout").hasAnyRole("MEMBRE", "ADMINISTRATEUR")
+				.requestMatchers("/profil","/modifierProfil","/ajout-vente","ajout","/logout","register","ajout-vente","/ajout-vente-valider").hasAnyRole("MEMBRE", "ADMINISTRATEUR")
 				.anyRequest().authenticated()
 			)
 			.formLogin((form) -> form
